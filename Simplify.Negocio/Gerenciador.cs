7a6703795564836d64d04b966a7213e35cfb2482
JoaoFernandees <joaovictors1999@gmail.com>
2018-05-18 18:43:12 -0300

    Gerenciador (Validação)
    Inserção de validações

--- Simplify.Negocio/Gerenciador.cs
+++ Simplify.Negocio/Gerenciador.cs
@@ -10,6 +10,7 @@ namespace Simplify.Negocio
     {
         private Banco banco = new Banco();
 
+        // ADICIONAR CLIENTE //
         public Validacao AdicionarCliente(Cliente clienteAdicionado)
         {
             Validacao validacao = new Validacao();
@@ -20,28 +21,74 @@ namespace Simplify.Negocio
                 validacao.Mensagens.Add("CPF", "O CPF deve conter 11 números");
             }
 
-            // Verifica existência do usuário no sistema //
-            if(this.banco.Clientes.Where(c => c.CPF == clienteAdicionado.CPF).Any() && validacao.Mensagens.Count == 0)
+            // Verifica existência do CPF já cadastado no sistema //
+            if (this.banco.Clientes.Where(c => c.CPF == clienteAdicionado.CPF).Any() && validacao.Mensagens.Count == 0)
             {
                 validacao.Mensagens.Add("CPF", "Cliente já cadastrado no sistema");
             }
 
             // Campo nome é obrigatório //
-            if(String.IsNullOrEmpty(clienteAdicionado.Nome))
+            if (String.IsNullOrEmpty(clienteAdicionado.Nome))
             {
                 validacao.Mensagens.Add("Nome", "O campo nome é obrigatório");
             }
 
-            // //
-            if(String.IsNullOrEmpty(clienteAdicionado.Email))
+            // Campo email é obrigatório//
+            if (String.IsNullOrEmpty(clienteAdicionado.Email))
             {
                 validacao.Mensagens.Add("Email", "O campo email é obrigatório");
             }
 
-            if(!clienteAdicionado.Email.Contains("@") && validacao.Mensagens.Count == 0)
+
+            // Campo email, inválido //
+            if (!clienteAdicionado.Email.Contains("@") && validacao.Mensagens.Count == 0)
             {
                 validacao.Mensagens.Add("Email", "Email inválido");
             }
+
+            // Verifica existência do email já cadastrado no sistema //
+            if (this.banco.Clientes.Where(c => c.Email == clienteAdicionado.Email).Any() && validacao.Mensagens.Count == 0)
+            {
+                validacao.Mensagens.Add("Email", "Email já cadastrado no sistema");
+            }
+
+            // Campo telefone é obrigatório //
+            if (String.IsNullOrEmpty(clienteAdicionado.Telefone))
+            {
+                validacao.Mensagens.Add("Telefone", "Campo telefone é obrigatório");
+            }
+
+            // Telefone deve conter 10 NÚMEROS //
+            if (clienteAdicionado.Telefone = 10)
+            {
+                validacao.Mensagens.Add("Telefone", "O telefone deve conter 12 números");
+            }
+
+            if (validacao.Valido)
+            {
+                this.banco.Clientes.Add(clienteAdicionado);
+                this.banco.SaveChanges();
+            }
+
+            return validacao;
+        }
+
+        // ALTERAR CLIENTE //
+        public Validacao AlterarCliente(Cliente clienteAlterado)
+        {
+            Validacao validacao = new Validacao();
+
+            return validacao;
+        }
+
+        // REMOVER CLIENTE//
+        public Validacao RemoverCliente(Cliente cliente)
+        {
+            Validacao validacao = new Validacao();
+            banco.Clientes.Remove(cliente);
+            banco.SaveChanges();
+
+            return validacao;
         }
     }
 }
