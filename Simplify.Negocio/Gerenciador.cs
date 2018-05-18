1a577259fc05983becbd1a155143dc32fc1002ca
JoaoFernandees <joaovictors1999@gmail.com>
2018-05-18 18:21:10 -0300

    Gerenciador (Validação)
    Inserção de algumas validações

--- Simplify.Negocio/Gerenciador.cs
+++ Simplify.Negocio/Gerenciador.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace Simplify.Negocio
 {
-    class Gerenciador
+    public class Gerenciador
     {
+        private Banco banco = new Banco();
+
+        public Validacao AdicionarCliente(Cliente clienteAdicionado)
+        {
+            Validacao validacao = new Validacao();
+
+            // CPF deve conter exatamente 11 NÚMEROS //
+            if (clienteAdicionado.CPF = 11)
+            {
+                validacao.Mensagens.Add("CPF", "O CPF deve conter 11 números");
+            }
+
+            // Verifica existência do usuário no sistema //
+            if(this.banco.Clientes.Where(c => c.CPF == clienteAdicionado.CPF).Any() && validacao.Mensagens.Count == 0)
+            {
+                validacao.Mensagens.Add("CPF", "Cliente já cadastrado no sistema");
+            }
+
+            // Campo nome é obrigatório //
+            if(String.IsNullOrEmpty(clienteAdicionado.Nome))
+            {
+                validacao.Mensagens.Add("Nome", "O campo nome é obrigatório");
+            }
+
+            // //
+            if(String.IsNullOrEmpty(clienteAdicionado.Email))
+            {
+                validacao.Mensagens.Add("Email", "O campo email é obrigatório");
+            }
+
+            if(!clienteAdicionado.Email.Contains("@") && validacao.Mensagens.Count == 0)
+            {
+                validacao.Mensagens.Add("Email", "Email inválido");
+            }
+        }
     }
 }
